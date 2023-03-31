7c2057092a95c4f89013538f576a3ce96e2f1313
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-03-31 01:45:56 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/_app.tsx
@@ -0,0 +1,34 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Liveblocks</title>
+        <meta name="robots" content="noindex" />
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        <link
+          href="https://liveblocks.io/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="https://liveblocks.io/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+export default App;
